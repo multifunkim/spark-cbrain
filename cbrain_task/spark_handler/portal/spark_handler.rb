2cdc8f77b3ab6f067c3c8847882e543b08937e34
Natacha Beck <natabeck@gmail.com>
2020-08-26 13:21:47 -0400

    [FIX] input file path

--- cbrain_task/spark_handler/portal/spark_handler.rb
+++ cbrain_task/spark_handler/portal/spark_handler.rb
@@ -122,6 +122,8 @@ class CbrainTask::SparkHandler < PortalTask
         # Change params.
         stage2_task.params                = params
         stage2_task.params[:jobs_indices] = job_index
+        stage2_task.params[:fmri]         = stage1_task.params[:fmri]
+        stage2_task.params[:mask]         = stage1_task.params[:mask]
         stage2_task.params[:out_dir]      = stage1_task.params[:out_dir]
 
         stage2_task.share_workdir_with(stage1_task)
@@ -163,6 +165,9 @@ class CbrainTask::SparkHandler < PortalTask
       stage3_task.tool_config_id = stage3_tc_id
       stage3_task.status         = 'New'
       stage3_task.params         = params
+      stage3_task.params[:fmri]  = stage2_task.params[:fmri]
+      stage3_task.params[:mask]  = stage2_task.params[:mask]
+
 
       stage3_task.share_workdir_with(stage2_task)
 
