c8db3b018b66a0256bf842fd2f82e7ed21d53482
Natacha Beck <natabeck@gmail.com>
2020-09-03 13:25:03 -0400

    [FIX] according to PR comments

--- cbrain_task/spark_handler/portal/spark_handler.rb
+++ cbrain_task/spark_handler/portal/spark_handler.rb
@@ -77,7 +77,8 @@ class CbrainTask::SparkHandler < PortalTask
         end
         # Add the new task to our tasklist
         currTask.save!
-        currTask.params[:out_dir] = currTask.params[:out_dir] + "-#{currTask.id}" + currTask.run_id
+        currTask.params[:out_dir]   = currTask.params[:out_dir] + "-#{currTask.id}" + currTask.run_id
+	currTask.params[:_cb_stage] = "1"
         currTask.save!
         stage1_tasks << currTask
       end
